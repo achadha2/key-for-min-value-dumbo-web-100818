f0147376d12acc7d510f64aa90a024102e0e3b14
andy.chadha2 <andy.chadha2@gmail.com>
2018-09-26 04:26:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,6 @@ def key_for_min_value(name_hash)
   min_key = 0
     name_hash.each do |key, value|
       if value < min_key || min_key == 0
-        
+        min_key = value
+        name = key
 end
